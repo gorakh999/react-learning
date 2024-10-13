0dadea1bc8f7374405681e7668c736589b5ef192
Gorakh <gorakhgupta999@gmail.com>
2024-10-13 15:00:00 +0530

    React Class  Components

--- day-06-oct-2024/my-app/src/App.js
+++ day-06-oct-2024/my-app/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
-      <Welcome name = "Ramesh"/>
-      <Welcome name = "Gorakh"/>
+      {/* <Welcome name = "Ramesh"/>
+      <Welcome name = "Gorakh"/> */}
+      <Greeting name='Gorakh'/>
+      <Greeting name='Ramesh'/>
+
       
     </div>
   );
