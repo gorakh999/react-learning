a0ec13f63448c13ad169d4130c4ebf8d3fe3519b
Gorakh <gorakhgupta999@gmail.com>
2024-10-13 15:08:46 +0530

    Import and Export Components

--- day-06-oct-2024/my-app/src/App.js
+++ day-06-oct-2024/my-app/src/App.js
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
 import Greeting from './components/Greeting';
+import DefaultComponent, { FirstComponent, SecondComponent } from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
       {/* <Welcome name = "Ramesh"/>
       <Welcome name = "Gorakh"/> */}
-      <Greeting name='Gorakh'/>
-      <Greeting name='Ramesh'/>
+      {/* <Greeting name='Gorakh'/>
+      <Greeting name='Ramesh'/> */}
+      <FirstComponent/>
+      <SecondComponent/>
+      <DefaultComponent/>
 
       
     </div>
---
+++ day-06-oct-2024/my-app/src/components/MyComponents.js
@@ -0,0 +1,13 @@
+export function FirstComponent(){
+    return <h1>First Component</h1>
+}
+
+export function SecondComponent(){
+    return <h1>Second Component</h1>
+}
+
+function DefaultComponent(){
+    return <h1>Default Component</h1>
+}
+
+export default DefaultComponent
