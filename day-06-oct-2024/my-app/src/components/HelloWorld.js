096a067846e05ad077eeef4779e73e5ab3ad75d0
Gorakh <gorakhgupta999@gmail.com>
2024-11-01 18:43:27 +0530

    JSX Rules

--- day-06-oct-2024/my-app/src/components/HelloWorld.js
+++ day-06-oct-2024/my-app/src/components/HelloWorld.js
@@ -2,8 +2,26 @@ import React from "react"
 
 const HelloWorld = () => {
 
+    function handleClick(){
+        alert("button clicked")
+    }
+
+    const name = "Ramesh"
+
+    const addition = 1 + 2;
+
     // const myElement = <h1>Hello World</h1>
     // return React.createElement('h1', null, 'Hello Gorakh')
-    return <h1>Hello World</h1>
+    return  (
+    <div>
+    <h1 className="title">Title</h1>
+    <h2>Heading 2</h2>
+    <p>This is a paragraph</p>
+    <p>{name}</p>
+    <p>{addition}</p>
+    <button onClick={handleClick}>Click Here</button>
+    </div>
+    )
+
 }
 export default HelloWorld
